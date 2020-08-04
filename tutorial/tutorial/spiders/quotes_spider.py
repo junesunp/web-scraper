0711effd175c6ae283a1a2ce9909aac51cbc78bb
Justin <49623256+justinshwang@users.noreply.github.com>
2020-08-04 18:17:17 -0400

    tutorial initial steps

---
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,10 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
